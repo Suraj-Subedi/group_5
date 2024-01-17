19cc38b144feb2c2a7d15d5dd8d5218a3c60cb0d
Suraj Subedi <soorajsubedi1227@gmail.com>
2024-01-17 12:33:58 +0545

    connction with php

--- android/app/src/main/kotlin/com/subedisuraj/ecom_5/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.subedisuraj.ecom_5
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ helpers/connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'ecom5';
+
+$CON = mysqli_connect($host, $user, $password, $db);
+
+
+if (!$CON) {
+    echo "Connection Failed";
+} else {
+    echo "Coneccted Successfully";
+}
